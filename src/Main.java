28948799eafd497ae527bdcabc918e11865bf216
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2022-12-12 12:59:26 +0600

    class practice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public boolean provideCoffee(CoffeeType coffeeType){
+        if(!isMaterialAvailable() || (!IsSugarPowderSufficient && (isCreamCoffee() || isSugerCoffee()))){
+            return false;
+        }
+    }
+
+    public boolean isMaterialAvailable(){
+        if(isMoneyAvailable() || isCoffeeThingAvailable())
+            return false;
+        else
+            return true;
+    }
+
+    public boolean isMoneyAvailable(){
+        if(_change < _cupPrice)
+            return true;
+    }
+
+    public boolean isCoffeeThingAvailable(){
+        if(!AreCupsSufficient || !IsCoffeePowderSufficient)
+            return true;
+    }
+
+    public boolean isCreamCoffee(){
+        if(coffeeType == coffeeType.Cream || isCreamSugerCoffee())
+            return true;
+    }
+
+    public boolean isSugerCoffee(){
+        if(coffeeType == coffeeType.Suger || isCreamSugerCoffee())
+            return true;
+    }
+
+    public boolean isCreamSugerCoffee(){
+        if(coffeeType == Coffeetype.CreamAndSugar)
+            return true;
     }
 }
