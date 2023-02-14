85583e9babaa83505c1b1c707e2e738be67653e7
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-02-14 15:58:58 +0600

    done

--- src/BehavioralDesignPattern/ObserverPattern/InClassAssignment/Main.java
+++ src/BehavioralDesignPattern/ObserverPattern/InClassAssignment/Main.java
@@ -5,24 +5,27 @@ import java.nio.file.StandardOpenOption;
 
 public class Main {
     public static void main(String[] args) {
-        ConcreteSubject subject = new ConcreteSubject("src/BehavioralDesignPattern/ObserverPattern/InClassAssignment/test.txt");
+        ConcreteSubject subject = new ConcreteSubject("src/BehavioralDesignPattern/ObserverPattern/InClassAssignment/Files/test.txt");
         subject.registerObserver(new ConcreteObserver("Monitor 1"));
         subject.registerObserver(new ConcreteObserver("Monitor 2"));
 
         // Simulate changes to the file being monitored
-        try {
-            Thread.sleep(1000);
-            // Modify the file
-            Files.write(subject.getFile().toPath(), "new content".getBytes(), StandardOpenOption.APPEND);
-            Thread.sleep(1000);
+//        try {
+//            Thread.sleep(1000);
+//            // Modify the file
+//            Files.write(subject.getFile().toPath(), "new content".getBytes(), StandardOpenOption.APPEND);
+//            Thread.sleep(1000);
 //            // Delete the file
 //            Files.delete(subject.getFile().toPath());
 //            Thread.sleep(1000);
 //            // Recreate the file
 //            Files.write(subject.getFile().toPath(), "recreated content".getBytes());
 //            Thread.sleep(1000);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        // or we can change the file being monitored manually
+        while(true) {}
     }
 }
